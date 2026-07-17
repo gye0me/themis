3bced3ef70b1294800f9ca985f6bbf151ebb8bc0
Your Name <your-email@example.com>
2026-07-17 23:46:02 +0900

    fix: package.json 버전 맞추기

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,6 @@ export default function (api) {
 
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: ['react-native-worklets/plugin'],
   }
 }
